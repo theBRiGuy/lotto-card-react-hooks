a41db0cfe17fb991296e1a7c26a590bb9d54d801
Brian Wolk <brian.wolk@sapientrazorfish.com>
2019-01-11 15:52:49 -0500

    Installed scss packages, LottoCard class now accepts props to create blank cards or use existing ones (future functionality)

---
+++ src/LottoCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LottoCard extends Component {
+  render() {
+    return (
+      <li className="LottoCardListItem">
+        <div className="LottoCard">LottoCard here</div>
+      </li>
+    );
+  }
+}
+
+export default LottoCard;
---
+++ src/LottoCardSet.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import LottoCard from './LottoCard';
+
+class LottoCardSet extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: this.props.cards || this.initBlankCards(this.props.numNewCards || 3) || []
+    };
+
+  }
+
+  initBlankCards(numCards) {
+    let cards = [];
+    for (let i = 0; i < numCards; i++) {
+      cards.push(<LottoCard />);
+    }
+    return cards;
+  }
+
+  render() {
+    const cards = () => {
+      if (this.state.cards.length > 0) {
+        return (
+          <ul className="LottoCardList">
+            {React.Children.toArray(this.state.cards)}
+          </ul>
+        );
+      } else {
+        return (
+          <p>No lotto cards!</p>
+        );
+      }
+    }
+    return (
+      <div className="LottoCardSet">
+        {cards()}
+      </div>
+    );
+  }
+}
+
+export default LottoCardSet;
