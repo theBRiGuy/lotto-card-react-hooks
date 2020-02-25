c2b0c40add5495975d3a71802536e153ac1ad4a6
Brian Wolk <brian.wolk@publicissapient.com>
2020-02-25 12:07:49 -0500

    Refactored LottoCardSet to use hooks

--- src/LottoCardSet.js
+++ src/LottoCardSet.js
@@ -1,59 +1,56 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import LottoCard from './LottoCard'
 
-class LottoCardSet extends Component {
+function LottoCardSet(props) {
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      cards: LottoCardSet.blankCards(this.props.initialCards)
-    }
-
-    this.handleAddCard = this.handleAddCard.bind(this)
-    this.handleRemoveCard = this.handleRemoveCard.bind(this)
-  }
-
-  static blankCards(n) {
-    const cards = []
+  // functions required for state
+  const blankCards = (n) => {
+    const cards = [];
     for (let i = 0; i < n; i++) {
       // Generate a unique card ID
-      cards.push({id: Date.now() + Math.random()})
+      cards.push({ id: Date.now() + Math.random() });
     }
-    return cards
+    return cards;
   }
 
-  handleAddCard() {
-    this.setState(oldState => ({ cards: [...oldState.cards, ...LottoCardSet.blankCards(1)]}))
+
+  // card state
+  const [cards, setCards] = useState(blankCards(props.initialCards));
+
+
+  // event handlers
+  const handleAddCard = () => {
+    setCards([...cards, ...blankCards(1)])
   }
 
-  handleRemoveCard(id) {
-    this.setState(oldState => ({ cards: oldState.cards.filter(item => item.id !== id)}))
+  const handleRemoveCard = (id) => {
+    // this.setState(oldState => ({ cards: oldState.cards.filter(item => item.id !== id)}))
+    setCards(cards.filter(card => card.id !== id))
   }
 
-  render() {
-    const noCards = (this.state.cards.length) ? null : <p className="LottoCardSet__noCards">No lotto cards!</p>
-    const addLottoCard = (
-      <li className="LottoCardListItem">
-        <button
-          className="LottoCardListItem__AddButton"
-          onClick={this.handleAddCard}
-        >
-          Click to<br />Add Card
-        </button>
-      </li>
-    )
+  // JSX functions
+  const noCards = (cards.length) ? null : <p className="LottoCardSet__noCards">No lotto cards!</p>
 
-    return (
-      <div className="LottoCardSet">
-        {noCards}
-        <ul className="LottoCardSet__lottoCardList">
-          {this.state.cards.map(card => <LottoCard handleRemoveCard={this.handleRemoveCard} key={card.id} id={card.id} maxNums={this.props.maxNums} />)}
-          {addLottoCard}
-        </ul>
-      </div>
-    )
-  }
+  const addLottoCard = (
+    <li className="LottoCardListItem">
+      <button
+        className="LottoCardListItem__AddButton"
+        onClick={handleAddCard}
+      >
+        Click to<br />Add Card
+      </button>
+    </li>
+  )
+
+  return (
+    <div className="LottoCardSet">
+      {noCards}
+      <ul className="LottoCardSet__lottoCardList">
+        {cards.map(card => <LottoCard handleRemoveCard={handleRemoveCard} key={card.id} id={card.id} maxNums={props.maxNums} />)}
+        {addLottoCard}
+      </ul>
+    </div>
+  )
 }
 
 export default LottoCardSet
