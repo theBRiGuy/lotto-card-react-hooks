f6ec47e0c1c42daf5d958626f3ee27414d04dca5
Brian Wolk <brian.wolk@publicissapient.com>
2020-02-25 12:41:40 -0500

    Refactored App to not use class anymore

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Landing from './Landing'
 import LottoCardSet from './LottoCardSet'
 import './App.scss'
 
-class App extends Component {
+function App() {
 
-  render() {
-    return (
-      <main>
-        <Switch>
-          <Route exact path='/landing' render={(props) =>
-            <Landing {...props} />
-          } />
-          <Route exact path='/' render={(props) =>
-            <LottoCardSet {...props} initialCards={2} maxNums={7} />
-          } />
-          <Route component={NoMatch} />
-        </Switch>
-      </main>
-    )
-  }
+  const NoMatch = () => <h1>Sorry, nothing to see here...</h1>;
+  
+  return (
+    <main>
+      <Switch>
+        <Route exact path='/landing' render={(props) =>
+          <Landing {...props} />
+        } />
+        <Route exact path='/' render={(props) =>
+          <LottoCardSet {...props} initialCards={2} maxNums={7} />
+        } />
+        <Route component={NoMatch} />
+      </Switch>
+    </main>
+  )
 }
-
-const NoMatch = () => (
-  <h1>Sorry, nothing to see here...</h1>
-)
 
 export default App
