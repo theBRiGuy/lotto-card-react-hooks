6abadb5429a256a89464502135b541d6be97fb59
Brian Wolk <brian.wolk@publicissapient.com>
2020-02-25 12:43:42 -0500

    Refactored LottoCardHeader to remove class

--- src/LottoCardHeader.js
+++ src/LottoCardHeader.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './LottoCardHeader.scss'
 
-class LottoCardHeader extends Component {
+function LottoCardHeader(props) {
 
-	render() {
-		return (
-			<div className="LottoCardHeader">
-				<ul className="LottoCardHeader__numList">
-					{this.props.selectedNums.map((num, i) => <li key={i} className="LottoCardHeader__numList__num">{num}</li>)}
-				</ul>
-				<ul className="LottoCardHeader__actionsList">
-					<li className="LottoCardHeader__actionsList__item--remove"><button onClick={this.props.handleRemoveCard}>Remove</button></li>
-				</ul>
-			</div>
-		)
-	}
+	return (
+		<div className="LottoCardHeader">
+			<ul className="LottoCardHeader__numList">
+				{props.selectedNums.map((num, i) => <li key={i} className="LottoCardHeader__numList__num">{num}</li>)}
+			</ul>
+			<ul className="LottoCardHeader__actionsList">
+				<li className="LottoCardHeader__actionsList__item--remove"><button onClick={props.handleRemoveCard}>Remove</button></li>
+			</ul>
+		</div>
+	)
 }
 
 export default LottoCardHeader
